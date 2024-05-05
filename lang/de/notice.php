05cd5a50662fdeada855b6ffff810afa733e03d7
Bettina von Mutius <vmutius@gmail.com>
2024-05-05 18:09:03 +0200

    Korrektur übersetzungen

--- lang/de/notice.php
+++ lang/de/notice.php
@@ -3,6 +3,6 @@
 return [
     'verify' => 'Validierung Ihrer Email Adresse',
     'verify_line1' => 'Um Ihren Account verwenden zu können, müssen Sie Ihre Email Adresse bestätigen.',
-    'verify_line2' => 'Eine Mail ist an Ihre Email Adresse gesendet worden. Bitte klicken Sie auf den Verifikationslink in der Mail.',
+    'verify_line2' => 'Eine Mail ist an Ihre Email Adresse gesendet worden. Dies kann ein paar Minuten dauern.Bitte klicken Sie auf den Verifikationslink in der Mail.',
     'verify_resend' => 'Verifikationslink erneut senden',
 ];
--- lang/en/notice.php
+++ lang/en/notice.php
@@ -3,6 +3,6 @@
 return [
     'verify' => 'Validate your email address',
     'verify_line1' => 'In order to use your account, you need to verify your email address',
-    'verify_line2' => 'An email has been sent to your email address. Please click on the verification link in the mail.',
+    'verify_line2' => 'An email has been sent to your email address. This may take a couple of minutes.Please click on the verification link in the mail.',
     'verify_resend' => 'Resend verification link',
 ];
