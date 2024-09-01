199bd3b96ab4e68b0e23c55fa6cf3656d6d42931
Saskia von Mutius <saskia.vonmutius@gmail.com>
2024-09-01 16:50:59 +0200

    EnclosureDarlehenPrivat

--- app/Livewire/Antrag/EnclosureFormDarlehenPrivat.php
+++ app/Livewire/Antrag/EnclosureFormDarlehenPrivat.php
@@ -30,25 +30,6 @@ class EnclosureFormDarlehenPrivat extends Component
 
     public $tax_assessment;
 
-    protected $rules = [
-        'enclosure.remark' => 'nullable',
-        'activity' => 'required_if:enclosure.activity,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.activity' => 'sometimes',
-        'activity_report' => 'required_if:enclosure.activity_report,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.activity_report' => 'sometimes',
-        'rental_contract' => 'required_if:enclosure.rental_contract,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.rental_contract' => 'sometimes',
-        'balance_sheet' => 'required_if:enclosure.balance_sheet,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.balance_sheet' => 'sometimes',
-        'tax_assessment' => 'required_if:enclosure.tax_assessment,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.tax_assessment' => 'sometimes',
-        'cost_receipts' => 'required_if:enclosure.cost_receipts,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.cost_receipts' => 'sometimes',
-        'open_invoice' => 'required_if:enclosure.open_invoice,null|mimes:png,jpg,jpeg,pdf|max:2048',
-        'enclosure.open_invoice' => 'sometimes',
-
-    ];
-
     public function validationAttributes(): array
     {
         return Lang::get('enclosure');
@@ -57,12 +38,12 @@ class EnclosureFormDarlehenPrivat extends Component
     public function rules()
     {
 
-        $activity = is_null($this->enclosure->activity) && $this->enclosure->activitySendLater==0;
-        $activity_report = is_null($this->enclosure->activity_report) && $this->enclosure->activityReportSendLater==0;
-        $rental_contract = is_null($this->enclosure->rental_contract) && $this->enclosure->rentalContractSendLater==0;
-        $balance_sheet = is_null($this->enclosure->balance_sheet) && $this->enclosure->balanceSheetSendLater==0;
-        $tax_assessment = is_null($this->enclosure->tax_assessment) && $this->enclosure->taxAssessmentSendLater==0;
-        $cost_receipts = is_null($this->enclosure->cost_receipts) && $this->enclosure->costReceiptsSendLater==0;
+        $activity = is_null($this->enclosure->activity) && $this->enclosure->activitySendLater == 0;
+        $activity_report = is_null($this->enclosure->activity_report) && $this->enclosure->activityReportSendLater == 0;
+        $rental_contract = is_null($this->enclosure->rental_contract) && $this->enclosure->rentalContractSendLater == 0;
+        $balance_sheet = is_null($this->enclosure->balance_sheet) && $this->enclosure->balanceSheetSendLater == 0;
+        $tax_assessment = is_null($this->enclosure->tax_assessment) && $this->enclosure->taxAssessmentSendLater == 0;
+        $cost_receipts = is_null($this->enclosure->cost_receipts) && $this->enclosure->costReceiptsSendLater == 0;
 
         return [
             'enclosure.remark' => 'nullable',
@@ -87,7 +68,7 @@ class EnclosureFormDarlehenPrivat extends Component
     {
         $lastname = auth()->user()->lastname;
         $firstname = auth()->user()->firstname;
-        $this->UserName = $lastname.'_'.$firstname;
+        $this->UserName = $lastname . '_' . $firstname;
         $this->enclosure = Enclosure::where('application_id', session()->get('appl_id'))
             ->first() ?? new Enclosure;
     }
@@ -152,7 +133,7 @@ class EnclosureFormDarlehenPrivat extends Component
     {
         if (! is_null($type)) {
             $appl_id = session()->get('appl_id');
-            $fileName = 'Appl'.$appl_id.'_'.$text.'.'.$type->getClientOriginalExtension();
+            $fileName = 'Appl' . $appl_id . '_' . $text . '.' . $type->getClientOriginalExtension();
 
             return $type->storeAs($this->UserName, $fileName, 's3');
         }
